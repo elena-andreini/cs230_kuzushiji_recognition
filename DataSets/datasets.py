d75456ad47e50a5e53f1ff39cfcf6707066b1f2b
elena-andreini <andreini.elena@gmail.com>
2024-11-18 17:07:29 +0100

    fixes to datasets.py

--- DataSets/datasets.py
+++ DataSets/datasets.py
@@ -1,4 +1,5 @@
 from torch.utils.data import DataLoader, Dataset
+from torchvision import transforms
 from ImageTools.custom_transforms import MinMaxNormalize
 
 class SimpleImageDataset(Dataset):
@@ -6,30 +7,7 @@ class SimpleImageDataset(Dataset):
         self.images = images
         #self.mean = np.mean(images)
         #self.stdev = np.std(images)
-        self.trivial_transform = transforms.Compose([
-            transforms.ToPILImage(),
-
-            transforms.ToTensor(),
-            MinMaxNormalize()])
-
-        self.transform = transforms.Compose([
-            transforms.ToPILImage(),
-            transforms.Resize((64, 64)),
-            #BinarizeImage(),
-            #Enhance(1.5),
-            #BlurAndBinarize(),
-            #transforms.ToPILImage(),
-            #transforms.Resize((28, 28)),  # Resize images to 28x28
-            transforms.ToTensor(),
-            #AdjustBrightnessContrast(brightness=0.2, contrast=0.2),
-            #AddGaussianNoise(mean=0.0, std=0.1),
-            #transforms.RandomRotation(5),
-            #transforms.RandomHorizontalFlip(),
-            #transforms.RandomCrop(28, padding=4),
-            MinMaxNormalize(),
-            ToRGB()
-            #transforms.Normalize(self.mean, self.stdev)
-        ])
+        self.transform = transform
 
     def __len__(self):
         return len(self.images)
