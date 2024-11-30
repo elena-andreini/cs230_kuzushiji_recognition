0b77e574c8d98794d38321cbd73394ba2384ae6f
elena-andreini <andreini.elena@gmail.com>
2024-11-30 19:40:57 +0100

    added utilis for freezing/unfreezing models

--- Training/training_utils.py
+++ Training/training_utils.py
@@ -21,3 +21,35 @@ def set_seed(seed):
     random.seed(seed)
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
+    
+def freeze_model(model):
+    """
+    Freezes all parameters in model.
+    Parameters:
+    model (torch.nn.Module): The model to be frozen.
+    """
+    for param in model.parameters():
+        param.requires_grad = False
+
+def unfreeze_last_n_modules(model, n):
+    """
+    Unfreezes the last n modules in the given model.
+
+    Parameters:
+    model (torch.nn.Module): The model to unfreeze.
+    n (int): Number of last modules to unfreeze.
+    """
+    # Get all modules in the model
+    modules = list(model.children())
+
+    # Check if n is greater than the number of modules
+    if n > len(modules):
+        print(f"Warning: The model only has {len(modules)} modules.")
+        n = len(modules)
+    print(f'the model has {modules} modules')
+    # Unfreeze the last n modules
+    for module in modules[-n:]:
+        for param in module.parameters():
+            param.requires_grad = True
+
+
