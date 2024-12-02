0ba7914856958dbdd78a078033ab5c3791044179
elena-andreini <andreini.elena@gmail.com>
2024-12-02 22:20:34 +0100

    removed logits from KuzushijiSimpleClassificationModel

--- Models/classification_models.py
+++ Models/classification_models.py
@@ -134,5 +134,5 @@ class KuzushijiSimpleClassificationModel(nn.Module):
         #char_features = self.fc(char_features) # code variation for single branch
         #char_features = F.relu(char_features) #code variation for single branch
         logits = self.classifier(combined_features)
-        output = F.softmax(logits, dim=1)  # Apply softmax to get probabilities
-        return output
+        #output = F.softmax(logits, dim=1)  # Apply softmax to get probabilities
+        return logits
